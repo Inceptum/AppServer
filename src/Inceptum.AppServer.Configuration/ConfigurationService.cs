e133235db5e5c2c573100df4e9f4ff2ff1be0488
Konstantin Nosov <knosov@inceptumsoft.com>
2011-10-01 20:20:45 +0400

    add wrapdes that was not in SC. ConfigurationService

---
+++ src/Inceptum.AppServer.Configuration/ConfigurationService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Inceptum.AppServer.Configuration
+{
+    public class ConfigurationService : IConfigurationService
+    {
+        private readonly IConfigurationProvider m_Provider;
+        private readonly IWebOperationContext m_Context;
+
+        public ConfigurationService(IConfigurationProvider provider)
+            : this(provider,new WebOperationContextWrapper())
+        {
+        }
+
+        internal ConfigurationService(IConfigurationProvider provider, IWebOperationContext context)
+        {
+            if (provider == null) throw new ArgumentNullException("provider");
+            if (context == null) throw new ArgumentNullException("context");
+            m_Provider = provider;
+            m_Context = context;
+        }
+
+
+        public Message GetBundle(string configuration, string bundleName, string parameters)
+        {
+            string[] extraParams = parameters.Split('/').ToArray();
+            string content;
+            try
+            {
+                content = m_Provider.GetBundle(configuration, bundleName, extraParams);
+            }
+            catch (Exception e)
+            {
+                throw new WebFaultException<string>(e.Message, HttpStatusCode.NotFound);
+            }
+            return m_Context.CreateTextResponse(content, "application/json; charset=utf-8", Encoding.UTF8);
+        }
+
+       
+
+        internal interface IWebOperationContext
+        {
+            Message CreateTextResponse(string content, string contentType, Encoding encoding);
+        }
+
+        
+
+        public class WebOperationContextWrapper : IWebOperationContext
+        {
+            #region IWebOperationContext Members
+
+            public Message CreateTextResponse(string content, string contentType, Encoding encoding)
+            {
+                return WebOperationContext.Current.CreateTextResponse(content, contentType, encoding);
+            }
+
+            #endregion
+        }
+ 
+    }
+}
--- src/Inceptum.AppServer.Configuration/LocalStorageConfigurationProvider.cs
+++ src/Inceptum.AppServer.Configuration/LocalStorageConfigurationProvider.cs
@@ -1,61 +1,16 @@
 ﻿using System;
 using System.Configuration;
 using System.Linq;
-using System.Net;
-using System.ServiceModel.Channels;
-using System.ServiceModel.Web;
-using System.Text;
 using Inceptum.AppServer.Configuration.Json;
 using Inceptum.AppServer.Configuration.Model;
 using Inceptum.AppServer.Configuration.Persistence;
 
 namespace Inceptum.AppServer.Configuration
 {
-    //TODO: extract ConfigurationService
     //TODO: better remoting implementation (MarshalByRefObject)
-    public class LocalStorageConfigurationProvider : MarshalByRefObject, IConfigurationProvider, IConfigurationService
+    public class LocalStorageConfigurationProvider : MarshalByRefObject, IConfigurationProvider
     {
-        #region WebOperationContext wrapper (tests backdooor)
-
-        private readonly IWebOperationContext m_Context;
-
-        internal LocalStorageConfigurationProvider(IConfigurationPersister persister, IContentProcessor contentProcessor,
-                                                   IWebOperationContext context)
-        {
-            m_Persister = persister;
-            m_ContentProcessor = contentProcessor;
-            m_Context = context;
-        }
-
-        #region Nested type: IWebOperationContext
-
-        internal interface IWebOperationContext
-        {
-            Message CreateTextResponse(string content, string contentType, Encoding encoding);
-        }
-
-        #endregion
-
-        #region Nested type: WebOperationContextWrapper
-
-        public class WebOperationContextWrapper : IWebOperationContext
-        {
-            #region IWebOperationContext Members
-
-            public Message CreateTextResponse(string content, string contentType, Encoding encoding)
-            {
-                return WebOperationContext.Current.CreateTextResponse(content, contentType, encoding);
-            }
-
-            #endregion
-        }
-
-        #endregion
-
-        #endregion
-
         private readonly IContentProcessor m_ContentProcessor;
-
         private readonly IConfigurationPersister m_Persister;
 
 
@@ -65,8 +20,6 @@ namespace Inceptum.AppServer.Configuration
         }
 
         public LocalStorageConfigurationProvider(IConfigurationPersister persister, IContentProcessor contentProcessor)
-            :
-                this(persister, contentProcessor, new WebOperationContextWrapper())
         {
             m_Persister = persister;
             m_ContentProcessor = contentProcessor;
@@ -76,7 +29,7 @@ namespace Inceptum.AppServer.Configuration
 
         public string GetBundle(string configuration, string bundleName, params string[] extraParams)
         {
-            var param=new[] { bundleName }.Concat(extraParams);
+            var param = new[] {bundleName}.Concat(extraParams);
             int paramLen = param.Count();
 
             Bundle bundle = null;
@@ -104,23 +57,10 @@ namespace Inceptum.AppServer.Configuration
 
         #endregion
 
-        #region IConfigurationService Members
-
-        public Message GetBundle(string configuration, string bundleName, string parameters)
+        public override object InitializeLifetimeService()
         {
-            var extraParams = parameters.Split('/').ToArray();
-            string content;
-            try
-            {
-                content = GetBundle(configuration, bundleName, extraParams);
-            }
-            catch (Exception e)
-            {
-                throw new WebFaultException<string>(e.Message,HttpStatusCode.NotFound);
-            }
-            return m_Context.CreateTextResponse(content, "application/json; charset=utf-8", Encoding.UTF8);
+            // prevents proxy from expiration
+            return null;
         }
-
-        #endregion
     }
 }
