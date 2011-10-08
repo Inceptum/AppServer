726086b296a3061b95e6ca88d671e069294093dc
knosov <devnull@localhost>
2011-10-08 15:27:35 +0400

    remote conf access

---
+++ src/Inceptum.AppServer.Configuration/Providers/LocalStorageConfigurationProvider.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using Inceptum.AppServer.Configuration.Json;
+using Inceptum.AppServer.Configuration.Model;
+using Inceptum.AppServer.Configuration.Persistence;
+
+namespace Inceptum.AppServer.Configuration.Providers
+{
+    //TODO: better remoting implementation (MarshalByRefObject)
+    public class LocalStorageConfigurationProvider : MarshalByRefObject, IConfigurationProvider
+    {
+        private readonly IContentProcessor m_ContentProcessor;
+        private readonly IConfigurationPersister m_Persister;
+
+
+        public LocalStorageConfigurationProvider(string configFolder) :
+            this(new FileSystemConfigurationPersister(configFolder), new JsonContentProcessor())
+        {
+        }
+
+        public LocalStorageConfigurationProvider(IConfigurationPersister persister, IContentProcessor contentProcessor)
+        {
+            m_Persister = persister;
+            m_ContentProcessor = contentProcessor;
+        }
+
+        #region IConfigurationProvider Members
+
+        public string GetBundle(string configuration, string bundleName, params string[] extraParams)
+        {
+            var param = new[] {bundleName}.Concat(extraParams);
+            int paramLen = param.Count();
+
+            Bundle bundle = null;
+
+            Config config;
+            try
+            {
+                config = m_Persister.Load(configuration, m_ContentProcessor);
+            }
+            catch (Exception e)
+            {
+                throw new ConfigurationErrorsException(string.Format("Failed to load configuration '{0}': {1}", configuration, e.Message));
+            }
+            while (bundle == null && paramLen != 0)
+            {
+                string name = string.Join(".", param.Take(paramLen));
+                bundle = config[name];
+                paramLen--;
+            }
+
+            if (bundle == null)
+                throw new ConfigurationErrorsException("Bundle not found");
+            return bundle.Content;
+        }
+
+        #endregion
+
+        public override object InitializeLifetimeService()
+        {
+            // prevents proxy from expiration
+            return null;
+        }
+    }
+}
---
+++ src/Inceptum.AppServer.Configuration/Providers/RemoteConfigurationProvider.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace Inceptum.AppServer.Configuration.Providers
+{
+    public class RemoteConfigurationProvider : ResourceConfigurationProviderBase
+    {
+        private const int DEFAULT_HELPROX_TIMEOUT = 30000;
+
+        private readonly int m_TimeoutInMs;
+
+        public RemoteConfigurationProvider(string configurationServiceUrl)
+            : this(configurationServiceUrl,  DEFAULT_HELPROX_TIMEOUT)
+        {
+        }
+
+        public RemoteConfigurationProvider(string configurationServiceUrl,  int timeoutInMs)
+        {
+            if (configurationServiceUrl == null) throw new ArgumentNullException("configurationServiceUrl");
+            if (!ValidationHelper.IsValidUrl(configurationServiceUrl)) throw new ArgumentException("Wrong URL format", "configurationServiceUrl");
+
+            m_TimeoutInMs = timeoutInMs;
+            configurationServiceUrl = configurationServiceUrl.Trim();
+            configurationServiceUrl = configurationServiceUrl.EndsWith("/") ? configurationServiceUrl : configurationServiceUrl + "/";
+            ConfifurationServiceUrl = new Uri(configurationServiceUrl);
+        }
+
+        public Uri ConfifurationServiceUrl { get; private set; }
+
+
+        protected internal override string GetResourceName(string configuration, string bundleName, params string[] extraParams)
+        {
+            return configuration+"/"+bundleName + "/" + string.Join("/", extraParams) + (extraParams.Length > 0 ? "/" : "");
+        }
+
+        protected internal override string GetResourceContent(string name)
+        {
+            var request = createRequest(name);
+
+            WebResponse response = null;
+            try
+            {
+                response = request.GetResponse();
+                using (var stream = response.GetResponseStream())
+                {
+                    Debug.Assert(stream != null);
+
+                    using (var streamReader = new StreamReader(stream))
+                    {
+                        return streamReader.ReadToEnd();
+                    }
+                }
+            }
+            catch (WebException webException)
+            {
+                response = webException.Response;
+            }
+            finally
+            {
+                if (response != null)
+                    response.Close();
+            }
+            return null;
+        }
+
+      
+        private WebRequest createRequest(string resourecName)
+        {
+            var uri = new Uri(ConfifurationServiceUrl, resourecName);
+            var request = WebRequest.Create(uri);
+            request.Proxy = new WebProxy();
+            request.Timeout = m_TimeoutInMs;
+
+            return request;
+        }
+    }
+}
