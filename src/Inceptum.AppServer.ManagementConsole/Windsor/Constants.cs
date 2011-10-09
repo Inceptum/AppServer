ce499d8971837a4e3cb806d2090e9e4ce2c5cd60
knosov <devnull@localhost>
2011-10-09 15:08:47 +0400

    missed files

--- src/Inceptum.AppServer.ManagementConsole/Windsor/Constants.cs
+++
@@ -1,7 +0,0 @@
-namespace Inceptum.AppServer.Management.Windsor
-{
-    internal static class Constants
-    {
-        internal const string REG_IS_INSTANCE_KEY = "is.instance.registration";
-    }
-}
