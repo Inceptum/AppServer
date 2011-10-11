a650bd30da492d6a4601dff74ed051d1a3b7a310
knosov <devnull@localhost>
2011-10-11 11:32:56 +0400

    sdk: connection string configuration

---
+++ src/Inceptum.AppServer.Sdk/Configuration/ConnectionString.cs
@@ -0,0 +1,90 @@
+﻿namespace Finam.Internet.Bank.Integration.Sdk.Windsor
+{
+    /// <summary>
+    /// Stores connection string. Dedicated type is requerid to simplefy IoC dependency resolving.
+    /// </summary>
+    public struct ConnectionString
+    {
+        private readonly string m_Value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConnectionString"/> struct.
+        /// </summary>
+        public ConnectionString(string connectionString)
+            : this()
+        {
+            m_Value = connectionString;
+        }
+
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="Finam.Internet.Bank.Integration.Sdk.Windsor.ConnectionString"/> to <see cref="System.String"/>.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
+        public static implicit operator string(ConnectionString connectionString)
+        {
+            return connectionString.ToString();
+        }
+
+
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.String"/> to <see cref="Finam.Internet.Bank.Integration.Sdk.Windsor.ConnectionString"/>.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
+        public static implicit operator ConnectionString(string connectionString)
+        {
+            return new ConnectionString(connectionString);
+        }
+
+        /// <summary>
+        /// Equalses the specified other.
+        /// </summary>
+        /// <param name="other">The other.</param>
+        /// <returns></returns>
+        public bool Equals(ConnectionString other)
+        {
+            return Equals(other.m_Value, m_Value);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object"/> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object"/> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (obj.GetType() != typeof (ConnectionString)) return false;
+            return Equals((ConnectionString) obj);
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return (m_Value != null ? m_Value.GetHashCode() : 0);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return m_Value;
+        }
+    }
+}
---
+++ src/Inceptum.AppServer.Sdk/Configuration/ConnectionStringResolver.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.Core;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Context;
+
+namespace Finam.Internet.Bank.Integration.Sdk.Windsor
+{
+    internal class ConnectionStringResolver : ISubDependencyResolver
+    {
+        private readonly Dictionary<string, ConnectionString> m_ConnectionStrings=new Dictionary<string, ConnectionString>();
+
+        public ConnectionStringResolver()
+        {
+        }
+
+        public ConnectionStringResolver(Dictionary<string, string> connectionStrings)
+        {
+            foreach (var str in connectionStrings)
+            {
+                m_ConnectionStrings.Add(str.Key, str.Value);
+            }
+
+
+
+        }
+
+
+        public bool CanResolve(CreationContext context, ISubDependencyResolver parentResolver,
+                               ComponentModel model, DependencyModel dependency)
+        {
+            return dependency.TargetItemType == typeof(ConnectionString) && m_ConnectionStrings.Any(p => p.Key.ToLower() == dependency.DependencyKey.ToLower());
+        }
+
+        public object Resolve(CreationContext context, ISubDependencyResolver parentResolver,
+                              ComponentModel model, DependencyModel dependency)
+        {
+            return m_ConnectionStrings.First(p => p.Key.ToLower() == dependency.DependencyKey.ToLower()).Value;
+        }
+    }
+}
