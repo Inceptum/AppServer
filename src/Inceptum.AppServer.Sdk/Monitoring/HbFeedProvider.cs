77cbf4cce302b8b4cce9c243de91e3fc23bd432e
mtripolitov <devnull@localhost>
2012-04-18 17:44:40 +0400

    IBANK-333 - Remove SonicEndpoint from Inceptum.AppServer

---
+++ src/Inceptum.AppServer.Sdk/Monitoring/HbFeedProvider.cs
@@ -0,0 +1,26 @@
+using Inceptum.Core.Messaging;
+using Inceptum.DataBus;
+using Inceptum.DataBus.Messaging;
+
+namespace Inceptum.AppServer.Monitoring
+{
+	/// <summary>
+	/// Hb feed provider
+	/// </summary>
+	[Channel(ServicesMonitor.HB_CHANNEL)]
+	public class HbFeedProvider : MessagingFeedProviderBase<HostHbMessage, EmptyContext>
+	{
+		private readonly Endpoint m_HbEndpoint;
+
+		public HbFeedProvider(IMessagingEngine messagingEngine, Endpoint hbEndpoint)
+			: base(messagingEngine)
+		{
+			m_HbEndpoint = hbEndpoint;
+		}
+
+		protected override Endpoint GetSubscriptionEndpoint(EmptyContext context)
+		{
+			return m_HbEndpoint;
+		}
+	}
+}
