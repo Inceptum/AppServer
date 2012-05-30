f4a68e50ba7c1ce6621a71c1c523e55dae0e6de6
mtripolitov <devnull@localhost>
2012-05-30 19:07:16 +0400

    Update ConnectionStringResolver to resolve IDictionary<string, ConnectionString>

--- src/Inceptum.AppServer.Sdk/Configuration/ConnectionStringResolver.cs
+++ src/Inceptum.AppServer.Sdk/Configuration/ConnectionStringResolver.cs
@@ -6,36 +6,44 @@ using Castle.MicroKernel.Context;
 
 namespace Inceptum.AppServer.Configuration
 {
-    internal class ConnectionStringResolver : ISubDependencyResolver
-    {
-        private readonly Dictionary<string, ConnectionString> m_ConnectionStrings=new Dictionary<string, ConnectionString>();
+	internal class ConnectionStringResolver : ISubDependencyResolver
+	{
+		private readonly Dictionary<string, ConnectionString> m_ConnectionStrings = new Dictionary<string, ConnectionString>();
 
-        public ConnectionStringResolver()
-        {
-        }
+		public ConnectionStringResolver()
+		{
+		}
 
-        public ConnectionStringResolver(Dictionary<string, string> connectionStrings)
-        {
-            foreach (var str in connectionStrings)
-            {
-                m_ConnectionStrings.Add(str.Key, str.Value);
-            }
+		public ConnectionStringResolver(Dictionary<string, string> connectionStrings)
+		{
+			foreach (var str in connectionStrings)
+			{
+				m_ConnectionStrings.Add(str.Key, str.Value);
+			}
+		}
 
+		public bool CanResolve(CreationContext context, ISubDependencyResolver parentResolver,
+		                       ComponentModel model, DependencyModel dependency)
+		{
+			if (dependency.TargetItemType == typeof (ConnectionString))
+				return m_ConnectionStrings.Any(p => p.Key.ToLower() == dependency.DependencyKey.ToLower());
+			
+			if (dependency.TargetItemType == typeof (IDictionary<string, ConnectionString>))
+				return m_ConnectionStrings.Any();
 
+			return false;
+		}
 
-        }
+		public object Resolve(CreationContext context, ISubDependencyResolver parentResolver,
+		                      ComponentModel model, DependencyModel dependency)
+		{
+			if (dependency.TargetItemType == typeof (ConnectionString))
+				return m_ConnectionStrings.First(p => p.Key.ToLower() == dependency.DependencyKey.ToLower()).Value;
 
+			if (dependency.TargetItemType == typeof (IDictionary<string, ConnectionString>))
+				return m_ConnectionStrings;
 
-        public bool CanResolve(CreationContext context, ISubDependencyResolver parentResolver,
-                               ComponentModel model, DependencyModel dependency)
-        {
-            return dependency.TargetItemType == typeof(ConnectionString) && m_ConnectionStrings.Any(p => p.Key.ToLower() == dependency.DependencyKey.ToLower());
-        }
-
-        public object Resolve(CreationContext context, ISubDependencyResolver parentResolver,
-                              ComponentModel model, DependencyModel dependency)
-        {
-            return m_ConnectionStrings.First(p => p.Key.ToLower() == dependency.DependencyKey.ToLower()).Value;
-        }
-    }
+			return null;
+		}
+	}
 }
