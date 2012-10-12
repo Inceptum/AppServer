b05b6977f773dfaeeb0d06fa99969e0a6f00c170
knosov <knosov@inceptumsoft.com>
2012-10-12 13:39:04 +0400

    configuration tests

---
+++ src/Inceptum.AppServer.Tests/Configuration/ResourceConfigurationProviderBaseTests.cs
@@ -0,0 +1,72 @@
+using System;
+using Inceptum.AppServer.Configuration.Providers;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Inceptum.Configuration.Tests.Client
+{
+    [TestFixture]
+    public class ResourceConfigurationProviderBaseTests
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetBundleNameIsNullFailureTest()
+        {
+            var provider = MockRepository.GeneratePartialMock<ResourceConfigurationProviderBase>();
+            provider.Expect(p => p.GetResourceContent("")).IgnoreArguments();
+            provider.Expect(p => p.GetResourceName("testConfiguration", "")).IgnoreArguments();
+
+            provider.GetBundle("testConfiguration", null);
+
+        }       
+        
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetBundleNameIsEmptyFailureTest()
+        {
+            var provider = MockRepository.GeneratePartialMock<ResourceConfigurationProviderBase>();
+            provider.Expect(p => p.GetResourceContent("")).IgnoreArguments();
+            provider.Expect(p => p.GetResourceName("testConfiguration", "")).IgnoreArguments();
+
+            provider.GetBundle("testConfiguration", "");
+
+        }
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetBundleEmptyParamFailureTest()
+        {
+            var provider = MockRepository.GeneratePartialMock<ResourceConfigurationProviderBase>();
+            provider.Expect(p => p.GetResourceContent("")).IgnoreArguments();
+            provider.Expect(p => p.GetResourceName("testConfiguration", "")).IgnoreArguments();
+
+            provider.GetBundle("testConfiguration", "bundle", "");
+
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetBundleNullParamFailureTest()
+        {
+            var provider = MockRepository.GeneratePartialMock<ResourceConfigurationProviderBase>();
+            provider.Expect(p => p.GetResourceContent("")).IgnoreArguments();
+            provider.Expect(p => p.GetResourceName("testConfiguration", "")).IgnoreArguments();
+
+            provider.GetBundle("testConfiguration", "bundle", "");
+
+        }
+
+
+        [Test]
+        public void GetBundleParamValuesAreTrimmedTest()
+        {
+            var provider = MockRepository.GeneratePartialMock<ResourceConfigurationProviderBase>();
+            provider.Expect(p => p.GetResourceContent("")).IgnoreArguments().Return("aa");
+            provider.Expect(p => p.GetResourceName("testConfiguration", "")).IgnoreArguments().Return("aa");
+
+            provider.GetBundle("testConfiguration", " bundle ", " test ");
+
+            provider.AssertWasCalled(p => p.GetResourceName("testConfiguration", "bundle", "test"));
+
+        }
+    }
+}
