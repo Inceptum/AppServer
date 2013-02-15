06c683e67db0a340eac9dfe17a381d92b9131093
Konstantin Nosov <knosov@inceptumsoft.com>
2013-02-15 19:25:34 +0400

    product info  attributes extracted to single file

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,5 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+
+[assembly: AssemblyCompany("InceptumSoft")]
+[assembly: AssemblyProduct("Inceptum.AppServer")]
+[assembly: AssemblyCopyright("Copyright © InceptumSoft 2013")]
+[assembly: AssemblyTrademark("Inceptum.AppServer")]
+[assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: InternalsVisibleTo("Inceptum.AppServer.Tests")]
