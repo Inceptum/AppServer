d1dae683b6dd202835dc6774d70281810d21e687
Konstantin Nosov <knosov@inceptumsoft.com>
2013-02-15 19:18:36 +0400

    versioning attributes extracted to single file

--- src/Inceptum.AppServer.Configuration/Properties/AssemblyInfo.cs
+++ src/Inceptum.AppServer.Configuration/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Inceptum.AppServer.Configuration")]
-[assembly: AssemblyDescription("")]
+/*[assembly: AssemblyDescription("")]*/
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Inceptum.AppServer.Configuration")]
