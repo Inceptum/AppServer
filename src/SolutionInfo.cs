e6e4691aad62c4d2651b0229da00bb7c834df7e1
Konstantin Nosov <knosov@inceptumsoft.com>
2013-02-15 18:52:48 +0400

    versioning attributes extracted to single file

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("Inceptum.AppServer.Tests")]
