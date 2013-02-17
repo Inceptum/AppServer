1dc3e8ffe49211dd40d42cb1a695a1531089ae92
Konstantin Nosov <knosov@inceptumsoft.com>
2013-02-17 16:01:56 +0400

    Logging initialization extracted to LoggingInstaller

---
+++ src/Inceptum.AppServer/Bootstrap/LoggingInstaller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Castle.Core.Logging;
+using Castle.Facilities.Logging;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Inceptum.AppServer.Logging;
+using Inceptum.AppServer.Notification;
+using Inceptum.Core.Utils;
+using NLog;
+using NLog.Config;
+
+namespace Inceptum.AppServer.Bootstrap
+{
+    public class LoggingInstaller:IWindsorInstaller
+
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            //nlog app domain resolver e.g. ${app_domain} processing in nlog.config
+            AppDomainRenderer.Register();
+            var logFolder = new[] { AppDomain.CurrentDomain.BaseDirectory, "logs", "server" }.Aggregate(Path.Combine);
+            //logfolder nlog variable
+            GlobalDiagnosticsContext.Set("logfolder", logFolder);
+            //Configuring management console target (forwards log to ui via SignalR)
+            ConfigurationItemFactory.Default.Targets.RegisterDefinition("ManagementConsole", typeof(ManagementConsoleTarget));
+            container.Register(
+                Component.For<ILogCache>().ImplementedBy<LogCache>().Forward<LogCache>().DependsOn(new { capacity = 2000 }),
+                Component.For<LogConnection>(),
+                Component.For<ManagementConsoleTarget>().DependsOn(new { source = "Server" }),
+                Component.For<UiNotificationHub>().Forward<IHostNotificationListener>()
+                );
+            //NLog has to resolve components from container
+            var createInstanceOriginal = ConfigurationItemFactory.Default.CreateInstance;
+            ConfigurationItemFactory.Default.CreateInstance = type => container.Kernel.HasComponent(type) ? container.Resolve(type) : createInstanceOriginal(type);
+            var nlogConf = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "nlog.config");
+            container.AddFacility<LoggingFacility>(f => f.LogUsing<GenericsAwareNLoggerFactory>().WithConfig(nlogConf));
+        }
+    }
+}
--- src/Inceptum.AppServer/Program.cs
+++ src/Inceptum.AppServer/Program.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.ServiceProcess;
+using Inceptum.AppServer.Bootstrap;
 using OpenWrap.Build;
 using OpenWrap.Commands;
 using OpenWrap.Commands.Wrap;
