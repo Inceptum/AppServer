5db73fc120545f9a05592753ae51d9b4b6373554
Konstantin Nosov <knosov@inceptumsoft.com>
2013-03-03 15:09:21 +0400

    Nuget for package management; Nuget app browser (incomplete)

---
+++ src/Inceptum.AppServer.Tests/NugetApplicationBrowserTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Inceptum.AppServer.AppDiscovery.Nuget;
+using NUnit.Framework;
+
+namespace Inceptum.AppServer.Tests
+{
+    [TestFixture]
+    public class NugetApplicationBrowserTests
+    {
+        [Test]
+        public void Test()
+        {
+            var assembly = GetType().Assembly;
+            var codebase = assembly.CodeBase.Replace("file:///", "");
+            var baseDir = Path.GetDirectoryName(codebase);
+            Directory.SetCurrentDirectory(baseDir);
+
+            var browser = new NugetApplicationBrowser();
+            var hostedAppInfos = browser.GetAvailabelApps().ToArray();
+            Console.WriteLine("");
+        }
+    }
+}
