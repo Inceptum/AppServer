27dfbec3bf88f4ddfe79543ed48bc57f535b081d
Konstantin Nosov <knosov@inceptumsoft.com>
2013-06-23 00:54:53 +0400

    some logging

--- src/Inceptum.AppServer/AppDiscovery/IApplicationBrowser.cs
+++ src/Inceptum.AppServer/AppDiscovery/IApplicationBrowser.cs
@@ -4,6 +4,7 @@ namespace Inceptum.AppServer.AppDiscovery
 {
     public interface IApplicationBrowser
     {
+        string Name { get;  }
         IEnumerable<HostedAppInfo> GetAvailabelApps();
     }
 }
