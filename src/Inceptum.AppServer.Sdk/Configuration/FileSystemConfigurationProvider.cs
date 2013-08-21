a83cf10286a2f14c3f6759cbbe76af74e0ec957a
Konstantin Nosov <knosov@inceptumsoft.com>
2013-08-21 11:07:40 +0400

    conf providers moved to SDK

---
+++ src/Inceptum.AppServer.Sdk/Configuration/FileSystemConfigurationProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Inceptum.AppServer.Configuration
+{
+    public class FileSystemConfigurationProvider:ResourceConfigurationProviderBase
+    {
+        private readonly string m_ConfigFolder;
+
+        public FileSystemConfigurationProvider(string configFolder)
+        {
+            if (configFolder == null) throw new ArgumentNullException("configFolder");
+            m_ConfigFolder = configFolder;
+            if (!Directory.Exists(configFolder))
+                Directory.CreateDirectory(configFolder);
+
+        }
+
+        protected internal override string GetResourceName(string configuration,string bundleName, params string[] extraParams)
+        {
+            return Path.Combine(m_ConfigFolder,configuration, bundleName + (extraParams.Length > 0 ? "." : "") + string.Join(".", extraParams));
+        }
+
+        protected internal override string GetResourceContent(string name)
+        {
+            return File.ReadAllText(name);
+        }
+
+        protected internal virtual void StoreBundle(string configuration,string bundleName, string[] extraParams, string content)
+        {
+            NormalizeParams(ref configuration,ref bundleName, extraParams);
+            var resourceName = GetResourceName(configuration, bundleName, extraParams);
+            var path = Path.Combine(m_ConfigFolder, resourceName);
+            var directoryName = Path.GetDirectoryName(path);
+            if (!Directory.Exists(directoryName))
+                Directory.CreateDirectory(directoryName);
+            if (File.Exists(path))
+            {
+                var fi = new FileInfo(path);
+                if ((fi.Attributes & FileAttributes.ReadOnly) > 0)
+                    fi.Attributes -= FileAttributes.ReadOnly;
+                fi.Delete();
+            }
+            File.WriteAllText(path, content);
+            
+        }
+    }
+}
