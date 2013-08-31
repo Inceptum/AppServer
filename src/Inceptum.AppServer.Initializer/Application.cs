631b17fd86fbfaf51f87bf437118274ca2005ede
Konstantin Nosov <knosov@inceptumsoft.com>
2013-08-31 19:41:38 +0400

    Console logging for hosted app

--- src/Inceptum.AppServer.Initializer/Application.cs
+++ src/Inceptum.AppServer.Initializer/Application.cs
@@ -97,44 +97,32 @@ namespace Inceptum.AppServer.Initializer
 
     internal static class Application
     {
-        static ManualResetEvent m_InstanceStopped = new ManualResetEvent(false);
+        static readonly ManualResetEvent m_InstanceStopped = new ManualResetEvent(false);
         private static ApplicationInitializer m_ApplicationInitializer;
         private static ServiceHost m_ServiceHost;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SetWindowText(IntPtr hWnd, string windowName);
+        [DllImport("kernel32.dll")]
+        private static extern bool SetConsoleTitle(string text);
 
         public static void Main(params string[] args)
         {
             System.Diagnostics.Debugger.Launch();
             var name = args[0];
-            Name = name;
             AppDomain.CurrentDomain.SetData("AppServer.Application",name);
             m_ApplicationInitializer = new ApplicationInitializer();
-            
-/*
-            var uri = "net.pipe://localhost/AppServer/" + PROCESS_BASIC_INFORMATION.GetParentProcess(Process.GetCurrentProcess().Handle).Id + "/ConfigurationProvider";
-            var factory=new ChannelFactory<IConfigurationProvider>(new NetNamedPipeBinding(),
-                new EndpointAddress(uri));
-            IConfigurationProvider provider = factory.CreateChannel();
-            var bundle=provider.GetBundle("AppServer", "server.host");
-*/
-       
-            
-         /*    var uri = "net.pipe://localhost/AppServer/" + PROCESS_BASIC_INFORMATION.GetParentProcess(Process.GetCurrentProcess().Handle).Id + "/instances/" + name+"Params";
-             var nonDuplexFactory = new ChannelFactory<IApplicationInstance>(new NetNamedPipeBinding(),new EndpointAddress(uri));
-             IApplicationInstance instance = nonDuplexFactory.CreateChannel();
-             var test=instance.Test();*/
 
+            IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
 
-/*
-             var factory = new DuplexChannelFactory<IApplicationInstance>(new ApplicationInitializer(), new NetNamedPipeBinding(),
-                   new EndpointAddress("net.pipe://localhost/AppServer/" + PROCESS_BASIC_INFORMATION.GetParentProcess(Process.GetCurrentProcess().Handle).Id + "/instances/"+name));
-*/
-            getCongigurationProvider();
-
-
-            var uri = "net.pipe://localhost/AppServer/" + PROCESS_BASIC_INFORMATION.GetParentProcess(Process.GetCurrentProcess().Handle).Id + "/instances/" + name;
+            SetConsoleTitle("AppServer - " + name); 
+            SetWindowText(handle, "AppServer - " + name);
+ 
+            var uri = "net.pipe://localhost/AppServer/" + WndUtils.GetParentProcess(Process.GetCurrentProcess().Handle).Id + "/instances/" + name;
             var factory = new ChannelFactory<IApplicationInstance>(new NetNamedPipeBinding(), new EndpointAddress(uri));
             IApplicationInstance instance = factory.CreateChannel();
             var instanceParams = instance.GetInstanceParams();
+            
 
             m_ApplicationInitializer.Initialize(instanceParams.ApplicationParams.AssembliesToLoad, instanceParams.ApplicationParams.NativeDllToLoad, null);
             var hostType = Type.GetType(instanceParams.AppHostType).MakeGenericType(Type.GetType(instanceParams.ApplicationParams.AppType));
@@ -143,7 +131,6 @@ namespace Inceptum.AppServer.Initializer
 
 
             var appHost = Activator.CreateInstance(hostType, getLogCache(), getCongigurationProvider(), instanceParams.Environment, name, instanceParams.AppServerContext);
-            Console.WriteLine("!!!");
 
             m_ServiceHost = new ServiceHost(appHost);
             var address = new Uri("net.pipe://localhost/AppServer/" + Process.GetCurrentProcess().Id + "/" + name);
@@ -151,25 +138,26 @@ namespace Inceptum.AppServer.Initializer
             m_ServiceHost.AddServiceEndpoint(hostType.GetInterface("IApplicationHost2"), new NetNamedPipeBinding(), address);
             //m_ConfigurationProviderServiceHost.Faulted += new EventHandler(this.IpcHost_Faulted);
             m_ServiceHost.Open();
-
+            
 
             instance.RegisterApplicationHost(address.ToString());
+            factory.Close();
             m_InstanceStopped.WaitOne();
 
         }
 
-        public static string Name { get; set; }
 
         private static IConfigurationProvider getCongigurationProvider()
         {
-            var uri = "net.pipe://localhost/AppServer/" + PROCESS_BASIC_INFORMATION.GetParentProcess(Process.GetCurrentProcess().Handle).Id + "/ConfigurationProvider";
+            var uri = "net.pipe://localhost/AppServer/" + WndUtils.GetParentProcess(Process.GetCurrentProcess().Handle).Id + "/ConfigurationProvider";
             var factory = new ChannelFactory<IConfigurationProvider>(new NetNamedPipeBinding(),
                 new EndpointAddress(uri));
             return factory.CreateChannel();
         }
+
         private static ILogCache getLogCache()
         {
-            var uri = "net.pipe://localhost/AppServer/" + PROCESS_BASIC_INFORMATION.GetParentProcess(Process.GetCurrentProcess().Handle).Id + "/LogCache";
+            var uri = "net.pipe://localhost/AppServer/" + WndUtils.GetParentProcess(Process.GetCurrentProcess().Handle).Id + "/LogCache";
             var factory = new ChannelFactory<ILogCache>(new NetNamedPipeBinding(),
                 new EndpointAddress(uri));
             return factory.CreateChannel();
@@ -177,95 +165,5 @@ namespace Inceptum.AppServer.Initializer
     }
 
 
-    /// <summary>
-    /// The proces s_ basi c_ information.
-    /// </summary>
-    [StructLayout(LayoutKind.Sequential)]
-    internal struct PROCESS_BASIC_INFORMATION
-    {
-        // These members must match PROCESS_BASIC_INFORMATION
-        /// <summary>
-        ///   The reserved 1.
-        /// </summary>
-        internal IntPtr Reserved1;
-
-        /// <summary>
-        ///   The peb base address.
-        /// </summary>
-        internal IntPtr PebBaseAddress;
-
-        /// <summary>
-        ///   The reserved 2_0.
-        /// </summary>
-        internal IntPtr Reserved2_0;
-
-        /// <summary>
-        ///   The reserved 2_1.
-        /// </summary>
-        internal IntPtr Reserved2_1;
-
-        /// <summary>
-        ///   The unique process id.
-        /// </summary>
-        internal IntPtr UniqueProcessId;
-
-        /// <summary>
-        ///   The inherited from unique process id.
-        /// </summary>
-        internal IntPtr InheritedFromUniqueProcessId;
-
-        /// <summary>
-        /// The nt query information process.
-        /// </summary>
-        /// <param name="processHandle">
-        /// The process handle. 
-        /// </param>
-        /// <param name="processInformationClass">
-        /// The process information class. 
-        /// </param>
-        /// <param name="processInformation">
-        /// The process information. 
-        /// </param>
-        /// <param name="processInformationLength">
-        /// The process information length. 
-        /// </param>
-        /// <param name="returnLength">
-        /// The return length. 
-        /// </param>
-        /// <returns>
-        /// The nt query information process. 
-        /// </returns>
-        [DllImport("ntdll.dll")]
-        private static extern int NtQueryInformationProcess(IntPtr processHandle, int processInformationClass, ref PROCESS_BASIC_INFORMATION processInformation, int processInformationLength, out int returnLength);
-
-        /// <summary>
-        /// Gets the parent process of a specified process.
-        /// </summary>
-        /// <param name="handle">
-        /// The process handle. 
-        /// </param>
-        /// <returns>
-        /// An instance of the Process class. 
-        /// </returns>
-        public static Process GetParentProcess(IntPtr handle)
-        {
-            PROCESS_BASIC_INFORMATION pbi = new PROCESS_BASIC_INFORMATION();
-            int returnLength;
-            int status = NtQueryInformationProcess(handle, 0, ref pbi, Marshal.SizeOf(pbi), out returnLength);
-            if (status != 0)
-            {
-                throw new Win32Exception(status);
-            }
-
-            try
-            {
-                return Process.GetProcessById(pbi.InheritedFromUniqueProcessId.ToInt32());
-            }
-            catch (ArgumentException)
-            {
-                // not found
-                return null;
-            }
-        }
-    }
+  
 }
---
+++ src/Inceptum.AppServer.Initializer/WndUtils.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Inceptum.AppServer.Initializer
+{
+    public class WndUtils
+    {
+        [DllImport("ntdll.dll")]
+        private static extern int NtQueryInformationProcess(IntPtr processHandle, int processInformationClass, ref PROCESS_BASIC_INFORMATION processInformation, int processInformationLength, out int returnLength);
+
+        /// <summary>
+        /// Gets the parent process of a specified process.
+        /// </summary>
+        /// <param name="handle">
+        /// The process handle. 
+        /// </param>
+        /// <returns>
+        /// An instance of the Process class. 
+        /// </returns>
+        public static Process GetParentProcess(IntPtr handle)
+        {
+            PROCESS_BASIC_INFORMATION pbi = new PROCESS_BASIC_INFORMATION();
+            int returnLength;
+            int status = NtQueryInformationProcess(handle, 0, ref pbi, Marshal.SizeOf(pbi), out returnLength);
+            if (status != 0)
+            {
+                throw new Win32Exception(status);
+            }
+
+            try
+            {
+                return Process.GetProcessById(pbi.InheritedFromUniqueProcessId.ToInt32());
+            }
+            catch (ArgumentException)
+            {
+                // not found
+                return null;
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// The proces s_ basi c_ information.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct PROCESS_BASIC_INFORMATION
+    {
+        // These members must match PROCESS_BASIC_INFORMATION
+        /// <summary>
+        ///   The reserved 1.
+        /// </summary>
+        internal IntPtr Reserved1;
+
+        /// <summary>
+        ///   The peb base address.
+        /// </summary>
+        internal IntPtr PebBaseAddress;
+
+        /// <summary>
+        ///   The reserved 2_0.
+        /// </summary>
+        internal IntPtr Reserved2_0;
+
+        /// <summary>
+        ///   The reserved 2_1.
+        /// </summary>
+        internal IntPtr Reserved2_1;
+
+        /// <summary>
+        ///   The unique process id.
+        /// </summary>
+        internal IntPtr UniqueProcessId;
+
+        /// <summary>
+        ///   The inherited from unique process id.
+        /// </summary>
+        internal IntPtr InheritedFromUniqueProcessId;
+
+        /// <summary>
+        /// The nt query information process.
+        /// </summary>
+        /// <param name="processHandle">
+        /// The process handle. 
+        /// </param>
+        /// <param name="processInformationClass">
+        /// The process information class. 
+        /// </param>
+        /// <param name="processInformation">
+        /// The process information. 
+        /// </param>
+        /// <param name="processInformationLength">
+        /// The process information length. 
+        /// </param>
+        /// <param name="returnLength">
+        /// The return length. 
+        /// </param>
+        /// <returns>
+        /// The nt query information process. 
+        /// </returns>
+     
+
+    }
+}
