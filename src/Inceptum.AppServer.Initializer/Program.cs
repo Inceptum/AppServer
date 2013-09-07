7d48ac55fbce4a95c5a3e79c860402f1f222c838
Konstantin Nosov <knosov@inceptumsoft.com>
2013-09-07 20:52:49 +0400

    aoo.config load in hosted process

--- src/Inceptum.AppServer.Initializer/Program.cs
+++ src/Inceptum.AppServer.Initializer/Program.cs
@@ -25,9 +25,11 @@ namespace Inceptum.AppServer.Initializer
         public static void Main(params string[] args)
         {
 #if !DEBUG
-            if (args.Length == 2 && args[1] == "-debug")
+            if (args.Length == 3 && args[2] == "-debug")
 #endif
                 Debugger.Launch();
+            AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", args[1]);
+
             var name = args[0];
             AppDomain.CurrentDomain.SetData("AppServer.Application",name);
             IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
