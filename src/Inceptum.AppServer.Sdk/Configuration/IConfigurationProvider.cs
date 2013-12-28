9ef3e29a8046499c0f3e6ea2487ce2124b190f86
Konstantin Nosov <knosov@inceptumsoft.com>
2013-12-28 14:55:24 +0400

    got rid off messaging dependency

--- src/Inceptum.AppServer.Sdk/Configuration/IConfigurationProvider.cs
+++ src/Inceptum.AppServer.Sdk/Configuration/IConfigurationProvider.cs
@@ -9,38 +9,5 @@ namespace Inceptum.AppServer.Configuration
         string GetBundle(string configuration, string bundleName, params string[] extraParams);
     }
 
-    public class ConfigurationInfo
-    {
-        public string Name { get; set; }
-        public BundleInfo[] Bundles { get; private set; }
 
-        public ConfigurationInfo()
-        {
-        }
-
-        public ConfigurationInfo(BundleInfo[] bundles)
-        {
-            Bundles = bundles;
-        }
-    }
-
-    public class BundleInfo
-    {
-        public string id { get; set; } 
-        public string Parent { get; set; } 
-        public string Name { get; set; } 
-        public string Configuration { get; set; } 
-        public string Content { get; set; }
-        public string PureContent { get; set; }
-        public BundleInfo[] Bundles { get; private set; }
-
-        public BundleInfo()
-        {
-        }
-
-        public BundleInfo(BundleInfo[] bundles)
-        {
-            Bundles = bundles;
-        }
-    }
 }
