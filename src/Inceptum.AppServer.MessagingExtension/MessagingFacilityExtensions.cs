68a1f9f8f101b829c1cc72ea4f224dadfb56d025
Konstantin Nosov <knosov@inceptumsoft.com>
2013-12-28 19:18:26 +0400

    naming fix

---
+++ src/Inceptum.AppServer.MessagingExtension/MessagingFacilityExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Castle.MicroKernel.Registration;
+using Inceptum.AppServer.Configuration;
+using Inceptum.Messaging.Castle;
+using Inceptum.Messaging.Configuration;
+
+namespace Inceptum.AppServer.Sdk.Messaging
+{
+
+
+    public static class MessagingFacilityExtensions
+    {
+
+        public static MessagingFacility ConfigureFromBundle(this MessagingFacility facility, string bundleName,
+            params string[] parameters)
+        {
+            facility.AddInitStep(k =>
+            {
+                k.Register(Component.For<IMessagingConfiguration>().ImplementedBy<MessagingConfiguration>().DependsOnBundle(bundleName, "", parameters));
+                facility.WithConfigurationFromContainer();
+            });
+            return facility;
+        }
+    }
+}
