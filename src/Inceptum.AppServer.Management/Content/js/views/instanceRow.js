e196299a7aaad4f79b8afb931f2bc981660f3257
Konstantin Nosov <k.nosov@unistream.com>
2014-01-07 17:43:33 +0400

    fixed bug where disabled buttons are clickable

--- src/Inceptum.AppServer.Management/Content/js/views/instanceRow.js
+++ src/Inceptum.AppServer.Management/Content/js/views/instanceRow.js
@@ -18,49 +18,55 @@ define([
                 "click .command":"emitCommand"
             },
 
-            'delete' : function(e){
+            'delete': function (e) {
                 e.preventDefault();
-                $(this.el).find(".actions button").attr("disabled", "disabled");
+                if ($(this.el).find(".delete").attr('disabled')) return false;
+                $(this.el).find(".actions button").addClass('disabled').attr('disabled','disabled');
                 this.trigger('destroy',this.model,this);
             },
             stop:function(e){
+                console.log($(e.target).prop('disabled'));
                 e.preventDefault();
-                $(this.el).find(".actions button").attr("disabled", "disabled");
+                if ($(this.el).find(".stop").attr('disabled')) return false;
+
+                $(this.el).find(".actions button").addClass('disabled').attr('disabled', 'disabled');
                 this.trigger('stop',this.model,this);
             },
             start:function(e){
                 e.preventDefault();
-                $(this.el).find(".actions button").attr("disabled", "disabled");
+                if ($(this.el).find(".start").attr('disabled')) return false;
+                $(this.el).find(".actions button").addClass('disabled').attr('disabled', 'disabled');
                 this.trigger('start',this.model,this);
             },
             restart:function(e){
                 e.preventDefault();
-                $(this.el).find(".actions button").attr("disabled", "disabled");
+                if ($(this.el).find(".restart").attr('disabled')) return false;
+                $(this.el).find(".actions button").addClass('disabled').attr('disabled','disabled');
                 this.trigger('restart',this.model,this);
             },
             emitCommand:function(e){
                 console.log($(e.target).data('command')) ;
                 e.preventDefault();
-                $(this.el).find(".actions button").attr("disabled", "disabled");
+                $(this.el).find(".actions button").addClass('disabled').attr('disabled','disabled');
                 this.trigger("command",this.model,this,$(e.target).data('command'));
             },
             render: function(){
                 this.template = _.template( template, { model: this.model.toJSON() } );
                 $(this.el).html(this.template);
-                $(this.el).find(".actions .btn.cmd").attr("disabled", "disabled");
+                $(this.el).find(".actions .btn.cmd").addClass('disabled').attr('disabled','disabled');
 
                 if(this.model.get("Status")=="Started")
                 {
-                    $(this.el).find(".restart").removeAttr("disabled");
-                    $(this.el).find(".stop").removeAttr("disabled");
-                    $(this.el).find(".delete").removeAttr("disabled");
+                    $(this.el).find(".restart").removeClass('disabled').removeAttr('disabled','disabled');
+                    $(this.el).find(".stop").removeClass('disabled').removeAttr('disabled','disabled');
+                    $(this.el).find(".delete").removeClass('disabled').removeAttr('disabled','disabled');
                 }
                 if(this.model.get("Status")=="Stopped")
                 {
-                    $(this.el).find(".start").removeAttr("disabled");
-                    $(this.el).find(".delete").removeAttr("disabled");
+                    $(this.el).find(".start").removeClass('disabled').removeAttr('disabled','disabled');
+                    $(this.el).find(".delete").removeClass('disabled').removeAttr('disabled','disabled');
                 }
-                $(this.el).find(".commands").removeAttr("disabled");
+                $(this.el).find(".commands").removeClass('disabled').removeAttr('disabled','disabled');
 
                 return this;
             },
