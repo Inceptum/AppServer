679a9251c34e82aaf9f6941878fdc4e5919f7137
Konstantin Nosov <knosov@inceptumsoft.com>
2014-04-05 18:36:23 +0400

    GatesRabbitMqConventionEndpointResolver introduced

--- src/Inceptum.AppServer.Sdk.Cqrs/AppServerAwareRabbitMqConventionEndpointResolver.cs
+++ src/Inceptum.AppServer.Sdk.Cqrs/AppServerAwareRabbitMqConventionEndpointResolver.cs
@@ -7,10 +7,18 @@ using Inceptum.Cqrs;
 
 namespace Inceptum.AppServer.Sdk.Cqrs
 {
+    public class GatesRabbitMqConventionEndpointResolver : AppServerAwareRabbitMqConventionEndpointResolver
+    {
+        public GatesRabbitMqConventionEndpointResolver(string transport, string serializationFormat, InstanceContext instanceContext) 
+            : base(transport, serializationFormat, instanceContext, "requests", "responses")
+        {
+        }
+    }
+
     public class AppServerAwareRabbitMqConventionEndpointResolver : RabbitMqConventionEndpointResolver
     {
-        public AppServerAwareRabbitMqConventionEndpointResolver(string transport, string serializationFormat, InstanceContext instanceContext)
-            : base(transport, serializationFormat, instanceContext.Name.Replace('.','-') + "." + instanceContext.AppServerName, instanceContext.Environment)
+        public AppServerAwareRabbitMqConventionEndpointResolver(string transport, string serializationFormat, InstanceContext instanceContext, string commandsKeyword = null, string eventsKeyword = null)
+            : base(transport, serializationFormat, instanceContext.Name.Replace('.', '-') + "." + instanceContext.AppServerName, instanceContext.Environment,commandsKeyword,eventsKeyword)
         {
         }
     }
