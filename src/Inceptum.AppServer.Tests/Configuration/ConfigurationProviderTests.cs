1ce522df47de12e2d22e2c9c793fa09dc442813d
Konstantin Nosov <knosov@inceptumsoft.com>
2014-08-10 18:14:15 +0400

    unit tests

--- src/Inceptum.AppServer.Tests/Configuration/ConfigurationProviderTests.cs
+++ src/Inceptum.AppServer.Tests/Configuration/ConfigurationProviderTests.cs
@@ -10,12 +10,28 @@ namespace Inceptum.Configuration.Tests.Client
     public class ConfigurationProviderTests
     {
         [Test]
+        public void AppServerConfigIsAlwaysLocalAndShouldBeNeverCachedTest()
+        {
+            var extProvider = MockRepository.GenerateMock<IManageableConfigurationProvider>();
+            var localProvider = MockRepository.GenerateMock<IManageableConfigurationProvider>();
+            localProvider.Expect(p => p.GetBundle(null, null, null)).IgnoreArguments().Return("a=b");
+            var fsProvider = MockRepository.GenerateMock<FileSystemConfigurationProvider>(".");
+            var provider = new CachingRemoteConfigurationProvider(fsProvider, extProvider, NullLogger.Instance, localProvider);
+            var bundle = provider.GetBundle("appserver", "test", "param1", "param2");
+            localProvider.AssertWasCalled((p => p.GetBundle("appserver", "test", "param1", "param2")));
+            extProvider.AssertWasNotCalled((p => p.GetBundle("appserver", "test", "param1", "param2")));
+            fsProvider.AssertWasNotCalled((p => p.GetBundle("appserver", "test", "param1", "param2")));
+            fsProvider.AssertWasNotCalled((p => p.StoreBundle("appserver", "test", new[] { "param1", "param2" }, "a=b")));
+            Assert.That(bundle, Is.EqualTo("a=b"), "wrong content was received");
+        }
+        [Test]
         public void LoadFromExternalProviderTest()
         {
             var extProvider = MockRepository.GenerateMock<IManageableConfigurationProvider>();
+            var localProvider = MockRepository.GenerateMock<IManageableConfigurationProvider>();
             extProvider.Expect(p => p.GetBundle(null, null, null)).IgnoreArguments().Return("a=b");
             var fsProvider = MockRepository.GenerateMock<FileSystemConfigurationProvider>(".");
-            var provider = new CachingRemoteConfigurationProvider(fsProvider, extProvider, NullLogger.Instance, extProvider);
+            var provider = new CachingRemoteConfigurationProvider(fsProvider, extProvider, NullLogger.Instance, localProvider);
             var bundle = provider.GetBundle("testConfiguration", "test", "param1", "param2");
             extProvider.AssertWasCalled((p => p.GetBundle("testConfiguration", "test", "param1", "param2")));
             fsProvider.AssertWasNotCalled((p => p.GetBundle("testConfiguration", "test", "param1", "param2")));
@@ -26,11 +42,12 @@ namespace Inceptum.Configuration.Tests.Client
         [Test]
         public void LoadFromCacheTest()
         {
+            var localProvider = MockRepository.GenerateMock<IManageableConfigurationProvider>();
             var extProvider = MockRepository.GenerateMock<IManageableConfigurationProvider>();
             extProvider.Expect(p => p.GetBundle(null, null, null)).IgnoreArguments().Return(null);
             var fsProvider = MockRepository.GenerateMock<FileSystemConfigurationProvider>(".");
             fsProvider.Expect(p => p.GetBundle(null, null, null)).IgnoreArguments().Return("a=b");
-            var provider = new CachingRemoteConfigurationProvider(fsProvider, extProvider, NullLogger.Instance, extProvider);
+            var provider = new CachingRemoteConfigurationProvider(fsProvider, extProvider, NullLogger.Instance, localProvider);
 
             var bundle = provider.GetBundle("testConfiguration","test", "param1", "param2");
 
