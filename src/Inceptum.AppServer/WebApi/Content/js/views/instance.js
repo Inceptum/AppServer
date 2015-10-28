a81b043e24aad00184c507798e42042f8c1214c4
Alex Bespalov <a.bespalov@unistream.com>
2015-10-28 11:06:31 +0300

    instancesList: -table-stripped +table-hover

--- src/Inceptum.AppServer/WebApi/Content/js/views/instance.js
+++ src/Inceptum.AppServer/WebApi/Content/js/views/instance.js
@@ -1,18 +1,19 @@
 define([
-    'jquery',
-    'backbone',
-    'underscore',
-    'text!templates/instance.html',
-    'views/serverSideBar'],
-    function($, Backbone, _,  template,ServerSideBarView){
+        'jquery',
+        'backbone',
+        'underscore',
+        'text!templates/instance.html',
+        'views/serverSideBar'
+    ],
+    function($, Backbone, _, template, ServerSideBarView) {
         var View = Backbone.View.extend({
             el: '#content',
-            initialize: function(){
+            initialize: function() {
             },
-            render: function(){
-                this.template = _.template( template, { model: this.model.toJSON() } );
+            render: function() {
+                this.template = _.template(template, { model: this.model.toJSON() });
                 $(this.el).html(this.template);
-                this.sidebar = new ServerSideBarView({ el: $(this.el).find(".sidebar"), active:"serverStatus" });
+                this.sidebar = new ServerSideBarView({ el: $(this.el).find(".sidebar"), active: "serverStatus" });
                 this.sidebar.render();
             }
         });
