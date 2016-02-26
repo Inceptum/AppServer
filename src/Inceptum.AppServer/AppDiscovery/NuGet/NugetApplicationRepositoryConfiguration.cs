8c94802679336dacbabd2545f64083f9eb5872f2
Konstantin Nosov <k.nosov@unistream.com>
2016-02-27 02:09:57 +0300

    lowest dependency version

--- src/Inceptum.AppServer/AppDiscovery/NuGet/NugetApplicationRepositoryConfiguration.cs
+++ src/Inceptum.AppServer/AppDiscovery/NuGet/NugetApplicationRepositoryConfiguration.cs
@@ -4,6 +4,10 @@ namespace Inceptum.AppServer.AppDiscovery.NuGet
 {
     internal class NugetApplicationRepositoryConfiguration
     {
+        public NugetApplicationRepositoryConfiguration()
+        {
+            DependencyVersion=global::NuGet.DependencyVersion.Lowest;
+        }
         public bool? AllowPrereleaseVersions { get; set; }
         public DependencyVersion? DependencyVersion { get; set; }
         public string ApplicationRepository { get; set; }
