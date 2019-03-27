c5a8b6c64ea828f6432acf834d707d48b13aed37
Mikhail Tripolitov <tripolitov@gmail.com>
2019-03-27 16:23:21 +0300

    Remove code of Cqrs SDK and Messaging SDK projects from Inceptum.AppServer solution
    - removed project `Inceptum.AppServer.Sdk.Messaging` from `Inceptum.AppServer` solution
    - deleted folder with `Inceptum.AppServer.Sdk.Messaging` project, code is moved to https://github.com/Inceptum/AppServer.Sdk.Messaging
    - removed project `Inceptum.AppServer.Sdk.Cqrs` from `Inceptum.AppServer` solution
    - deleted folder with `Inceptum.AppServer.Sdk.Cqrs` project, code is moved to https://github.com/Inceptum/AppServer.Sdk.Cqrs

--- src/Inceptum.AppServer.Sdk.Messaging/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Inceptum.AppServer.MessagingExtension")]
-[assembly: ComVisible(false)]
-[assembly: Guid("05c0c478-01d9-4f30-b28e-420eb7e17464")]
